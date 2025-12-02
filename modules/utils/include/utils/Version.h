6bf66958bcd6878520f5332aaf88073f9972106f
Umit Sert <sert.umit@gmail.com>
2025-12-02 06:53:41 +0300

    02-12-2025 06:53:22-Salı-> versiyon değiştirme v12.220.01

--- modules/utils/include/utils/Version.h
+++ modules/utils/include/utils/Version.h
@@ -5,6 +5,6 @@
 namespace recum12::utils {
 
 inline constexpr std::string_view APP_NAME    = "ReCUm12->";
-inline constexpr std::string_view APP_VERSION = "v12.210.03";
+inline constexpr std::string_view APP_VERSION = "v12.220.01";
 
 } // namespace recum12::utils
